7081c49055c0a7e8ec785bf242f68ca61b513a83
Emmar Caber <caberemmar@gmail.com>
2024-02-21 20:46:55 +0800

    feat: modify fillables of each models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
+        'position_id',
+        'role',
         'password',
     ];
 
