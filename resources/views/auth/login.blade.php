a5f4078856e4ff16f930cf31e5cf8a8143058711
Emmar Caber <caberemmar@gmail.com>
2024-02-21 23:50:26 +0800

    style: change some comments in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Username -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
             <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required
