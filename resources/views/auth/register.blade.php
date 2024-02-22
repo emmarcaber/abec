51b01cf7fc33efcaa5cae9b7d1574a0f9fb6e124
Emmar Caber <caberemmar@gmail.com>
2024-02-22 13:09:17 +0800

    feat: make position dropdown required

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
                     Committee</label>
             </div>
 
-            <select id="position" name="position_id"
+            <select id="position" name="position_id" required
                 class="block w-full mt-2 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
             </select>
         </div>
