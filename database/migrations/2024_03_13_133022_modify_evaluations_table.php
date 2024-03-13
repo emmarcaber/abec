be46aa1d87d284e537b7e2179b38941ee7280537
Emmar Caber <caberemmar@gmail.com>
2024-03-13 21:37:58 +0800

    feat: add columns in evaluations table

---
+++ database/migrations/2024_03_13_133022_modify_evaluations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('evaluations', function (Blueprint $table) {
+            $table->unsignedTinyInteger('knowledge_expertise')->after('user_id');
+            $table->unsignedTinyInteger('leadership')->after('knowledge_expertise');
+            $table->unsignedTinyInteger('teamwork_collaboration')->after('leadership');
+            $table->unsignedTinyInteger('work_ethic_dedication')->after('teamwork_collaboration');
+            $table->unsignedTinyInteger('overall_contribution_to_team')->after('work_ethic_dedication');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
