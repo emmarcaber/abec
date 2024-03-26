bfa285a3d3ee240a25c8396251f42b42e91b8fd4
Emmar Caber <caberemmar@gmail.com>
2024-03-26 22:33:20 +0800

    fix: close some unclosed tags

--- resources/views/users/evaluations/index.blade.php
+++ resources/views/users/evaluations/index.blade.php
@@ -78,7 +78,7 @@
                                             class="px-6 py-4 flex sm:flex-row flex-col justify-center items-center gap-4">
                                             <button data-modal-target="view-modal-{{ $evaluation->id }}"
                                                 data-modal-toggle="view-modal-{{ $evaluation->id }}" type=button
-                                                class="font-medium text-cyan-600 dark:text-cyan-500">
+                                                class="font-medium text-blue-600 dark:text-blue-500 rounded-md focus:ring-4 focus:ring-blue-300">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                                     class="w-6 h-6">
@@ -88,17 +88,20 @@
                                                         d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                                 </svg>
                                             </button>
-                                            <a href="#" class="font-medium text-amber-600 dark:text-amber-500">
+                                            <button type="button" data-modal-target="edit-modal-{{ $evaluation->id }}"
+                                                data-modal-toggle="edit-modal-{{ $evaluation->id }}"
+                                                class="font-medium text-amber-600 dark:text-amber-500 rounded-md focus:ring-4 focus:ring-amber-300">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                                     class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                         d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                                 </svg>
-                                            </a>
+                                            </button>
                                         </td>
 
                                         <x-evaluations.view-modal :$evaluation />
+                                        <x-evaluations.edit-modal :$evaluation />
                                     </tr>
                                 @endforeach
                             </tbody>
