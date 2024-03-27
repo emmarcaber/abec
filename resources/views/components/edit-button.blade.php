ae25002b99366ac754ce9a89688e72638a594e5f
Emmar Caber <caberemmar@gmail.com>
2024-03-27 21:56:51 +0800

    feat: change hover bgcolor of edit button

--- resources/views/components/edit-button.blade.php
+++ resources/views/components/edit-button.blade.php
@@ -1,4 +1,4 @@
 <a
-    {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-amber-500 dark:bg-amber-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-amber-500 uppercase tracking-widest hover:bg-amber-700 dark:hover:bg-white focus:bg-amber-700 dark:focus:bg-white active:bg-amber-900 dark:active:bg-amber-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-500 transition ease-in-out duration-150 cursor-pointer']) }}>
+    {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-amber-500 dark:bg-amber-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-amber-500 uppercase tracking-widest hover:bg-amber-600 dark:hover:bg-white focus:bg-amber-600 dark:focus:bg-white active:bg-amber-600 dark:active:bg-amber-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-500 transition ease-in-out duration-150 cursor-pointer']) }}>
     {{ $slot }}
 </a>
